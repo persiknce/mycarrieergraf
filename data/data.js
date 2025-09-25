d1fc4f12b76dde32690dc6793e72e1712149be9b
persiknce <persikbence@gmail.com>
2025-09-25 14:38:07 +0200

    Create data.js
    datas

---
+++ data/data.js
@@ -0,0 +1,34 @@
+subjectsData = {
+    CurriculumType: "Simple",
+    MaxCredit: 210,
+    Groups:[
+        {
+            type: "torzs",
+            name: "torzsanyag",
+            fullname: "Törzstárgyak",
+            subjects:[  ]
+        },         
+        {
+            type: "agazat",
+            name: "repulo",
+            fullname:"Repülőmérnöki szak", 
+            subjects:[
+                {
+                    name: "Solid edge",
+                    kategoria: "CAD",
+                },
+                {
+                    name: "3d printing",
+                    kategoria: "CAD",
+                },
+                {
+                    name: "system knowledge",
+                    kategoria: "CAD",
+                },
+                                        
+                      ],
+            specializations:[
+            ]
+        },         
+    ]    
+}
